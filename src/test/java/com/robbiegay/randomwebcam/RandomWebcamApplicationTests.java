095689e1fb65ebb181058f98c320a22a0cdc070e
Robbie Gay <robbiecgay@gmail.com>
2021-06-16 10:07:07 -0400

    Initial commit

---
+++ src/test/java/com/robbiegay/randomwebcam/RandomWebcamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robbiegay.randomwebcam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomWebcamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
